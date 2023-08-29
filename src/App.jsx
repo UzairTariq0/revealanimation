df5137a6db1dd746c2e73f2ae6b3d0d439352d26
Uzair Tariq <uzair.t2006@gmail.com>
2023-08-29 14:52:46 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Reveal } from './Reveal'
+function App() {
+
+  return (
+    <div>
+      <Reveal>
+        <h4>HTML, CSS and Javascript</h4>
+      </Reveal>
+    </div>
+  )
+}
+
+export default App
